7b71645a8d307e3dd7897d3c1fbb9768c635c42b
Abdoul Magomadov <amagomad@c4r2p2.42nice.fr>
2024-05-16 23:13:08 +0200

    printf

---
+++ ft_printf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amagomad <amagomad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 15:00:10 by amagomad          #+#    #+#             */
+/*   Updated: 2024/05/16 22:59:37 by amagomad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_putchar_fd(char c, int fd);
+int	ft_putstr_fd(char *s, int fd);
+int	ft_putnbr_fd(int n, int fd);
+int	ft_uputnbr_fd(unsigned int n, int fd);
+int	ft_puthex_fd(unsigned long long n, int fd, int uppercase);
+int	ft_putptr_fd(void *ptr, int fd);
+
+#endif
---
+++ ft_puthex_fd.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthex_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amagomad <amagomad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 22:08:23 by amagomad          #+#    #+#             */
+/*   Updated: 2024/05/16 23:08:08 by amagomad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_puthex_fd(unsigned long long n, int fd, int uppercase)
+{
+	char	*base;
+	int		count;
+
+	count = 0;
+	if (uppercase)
+	{
+		base = "0123456789ABCDEF";
+	}
+	else
+	{
+		base = "0123456789abcdef";
+	}
+	if (n >= 16)
+	{
+		count += ft_puthex_fd(n / 16, fd, uppercase);
+	}
+	count += ft_putchar_fd(base[n % 16], fd);
+	return (count);
+}
